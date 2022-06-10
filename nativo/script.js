d0ce1829dd1c1bf324ec1715395b7f0016156ad4
Bruno Costa <analistabrunocosta@gmail.com>
2022-06-09 21:02:11 -0300

    implementando visualização da api no HTML

--- nativo/script.js
+++ nativo/script.js
@@ -1,3 +1,6 @@
+let lista = document.querySelector("#lista")
+let tabela = document.querySelector("#tabela")
+
 async function carregarDados(){
     const url = "https://swapi.dev/api/people/?format=json"
     try {
@@ -6,7 +9,38 @@ async function carregarDados(){
         const dados = await resultado.json()
 
         for(elementos of dados.results){
-            console.log(`Sou ${elementos.name} nasci no ano ${elementos.birth_year}`)
+            //console.log(`Sou ${elementos.name} nasci no ano ${elementos.birth_year}`)
+            console.log(elementos)
+            //Criando elementos HTML dinamicamente
+            /*
+            const itemLista = document.createElement("li")
+            itemLista.classList.add("list-group-item")
+            itemLista.textContent = `Olá, sou ${elementos.name}, meu gênereo é ${elementos.gender} e a cor do meu cabelo é ${elementos.hair_color}`
+
+            lista.appendChild(itemLista)
+            */
+
+            // Criando elementos HTML
+            const linha = document.createElement("tr")
+            const tdNome = document.createElement("td")
+            const tdPeso = document.createElement("td")
+            const tdCorOlho = document.createElement("td")
+
+            // Criando conteúdo das tds
+            tdNome.textContent = elementos.name
+            tdPeso.textContent = elementos.mass
+            tdCorOlho.textContent = elementos.eye_color
+
+            // Adicionando os elementos em suas respectivas tags pai/mãe
+            linha.appendChild(tdNome)
+            linha.appendChild(tdPeso)
+            linha.appendChild(tdCorOlho)
+
+            tabela.appendChild(linha)
+
+
+
+
         }
     
         //console.log(dados.results)
